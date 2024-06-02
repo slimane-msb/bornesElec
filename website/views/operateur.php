a80ba4e0ace548e9188413823402abf2d4afc84a
slimane-msb <slimanemesbah0@gmail.com>
2024-06-02 21:26:25 +0200

    clean ope

--- website/views/operateur.php
+++ website/views/operateur.php
@@ -4,6 +4,69 @@
     loginFirst();
     include("includes/head.php");
     include("includes/navigation.php");
+
+    function doesExistOperateur($idOperateur, $conn){
+        $req = "SELECT * FROM operateurs WHERE numeroId = '$idOperateur'";
+        $result = $conn->query($req);
+        if ($result->num_rows > 0) {
+            return True;
+        }else{
+            return False;
+        }
+
+    }
+
+    function editOperateur($valeurArray, $conn){
+        $idOperateur = $valeurArray['idOperateur'];
+        if (doesExistOperateur($idOperateur,$conn )){
+            $req = "UPDATE operateurs SET ";
+            
+            $nbElement = 0;
+            foreach ($valeurArray as $colonne => $valeur) {
+                if($valeur != "" && $colonne!="idOperateur" && $colonne!="formName" ){
+                    $nbElement++;
+                    $req .= " $colonne = '$valeur', ";
+                }
+            }
+            $req = substr($req, 0, -2);
+            $req .= " WHERE numeroId = $idOperateur";
+            
+            if($nbElement>0){
+                $conn->query($req);
+                return createLog(False,"Modifié avec succès"); 
+            }else{
+                return createLog(True,"Veuillez entrer un element a modifier"); 
+            }
+        }else{
+            return createLog(True,"Operateur n'existe pas");
+        }
+
+    }
+
+    function deleteOperateur($idOperateur, $conn){
+        if (doesExistOperateur($idOperateur, $conn)){
+            $req = "DELETE FROM operateurs WHERE numeroId = $idOperateur";
+            $conn->query($req);
+            return createLog(False,"Supprimé avec succès"); 
+        }else{
+            return createLog(True,"Operateur n'existe pas");
+        }
+        
+    }
+
+
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        include("db_connect.php");
+        $conn = connectDb("BORNES2", $dbuser, $dbpass);
+
+        if ($_GET['formName'] == 'edit') {
+            $editLog = editOperateur($_GET, $conn);
+        } elseif ($_GET['formName'] == 'delete') {
+            $deleteLog = deleteOperateur($_GET['idOperateur'], $conn );
+        } 
+        disconnectDb($conn);
+    } 
+
 ?>
 
 
@@ -11,7 +74,10 @@
 <div class="page pageAsRow">
     <div class="login-container">
         <h2>Modifier un operateur</h2>
-        <form action="operateurEdit.php" name="editOperateur" method="GET" > 
+
+        <?php showLog($editLog); ?>
+
+        <form name="editOperateur" method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>"> 
             <input type="hidden" name="formName" value="edit"/>
             <input type="text" name="idOperateur" id="idOperateur" placeholder="Identifiant" required>
             <input type="text" name="nom" id="nom" placeholder="Nom" >
@@ -25,7 +91,10 @@
 
     <div class="login-container">
         <h2>Supprimer un operateur</h2>
-        <form action="operateurEdit.php" name="supprimerOperateur" method="GET" > 
+
+        <?php showLog($deleteLog); ?>
+
+        <form name="supprimerOperateur" method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>" > 
             <input type="hidden" name="formName" value="delete"/>
             <input type="text" name="idOperateur" id="idOperateur" placeholder="Identifiant" required>
             <button type="supprimerOperateur">Supprimer Un Operateur</button>
